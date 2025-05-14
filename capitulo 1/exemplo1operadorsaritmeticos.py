d005428985f8948ac37119dd495581fa5dfebddc
João <j.cruz10@escola.pr.gov.br>
2025-05-14 10:33:34 -0300

    Arrrumado

---
+++ capitulo 1/exemplo1operadorsaritmeticos.py
@@ -0,0 +1,15 @@
+# operador de soma
+soma = 1 + 2
+
+# operador de subtracao
+subtracao = 2 - 1
+
+# operador de multiplicacao
+multiplicacao = 2 * 1
+
+# operador de divisão
+divisao = 2 / 1
+
+# operador de exponenciacao (potencia)
+exponenciacao = 2 ** 1
+
