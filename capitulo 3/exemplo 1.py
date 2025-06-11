29863f9017303c1b39411d0c11baf0fcfd100188
João <j.cruz10@escola.pr.gov.br>
2025-06-11 10:58:00 -0300

    exemplo 1

---
+++ capitulo 3/exemplo 1.py
@@ -0,0 +1,24 @@
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+def subtracao(numero1, numero2):
+    return numero1 - numero2
+
+def multiplicacao(numero1, numero2):
+    return numero1 * numero2
+
+def divisao(numero1, numero2):
+    return numero1 / numero2
+
+numero1 = int(input("Digite um numero inteiro!"))
+numero2 = int(input("Digite um numero inteiro!"))
+
+resultadoSoma = soma(numero1, numero2)
+resultadoSubtracao = subtracao(numero1, numero2)
+resultadoMultiplicacao = multiplicacao(numero1, numero2)
+resultadoDivisao = divisao(numero1, numero2)
+
+print("O resultado da funcao soma", resultadoSoma)
+print("O resultado da funcao subtracao", resultadoSubtracao)
+print("O resultado da funcao multiplicacao", resultadoMultiplicacao)
+print("O resultado da funcao divisao", resultadoDivisao)
