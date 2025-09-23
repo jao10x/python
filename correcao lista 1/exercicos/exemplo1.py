e0800b28d0bf05671a8f7d8d47cea31d1192d9df
JOÃO GUILHERME <j.cruz10@escola.pr.gov.br>
2025-09-23 11:52:21 -0300

    exercicios

---
+++ correcao lista 1/exercicos/exemplo1.py
@@ -0,0 +1,10 @@
+def apresentacao(nome, nomepai, nomemae):
+    print(f"nome: {nome}")
+    print(f"nomepai: {nomepai}")
+    print(f"nomemae: {nomemae}")
+
+nome = input("digite seu nome")
+nomepai = input("digite do seu pai")
+nomemae = input("digite nome da sua mae")
+
+apresentacao(nome, nomepai, nomemae)
